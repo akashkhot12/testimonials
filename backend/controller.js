86811d87cb9724157ec3a231772ca36073189401
akashkhot12 <akashkhot03@gmail.com>
2024-07-04 16:02:53 +0530

    adding testimonial

--- backend/controller.js
+++ backend/controller.js
@@ -1,22 +1,23 @@
-const connetions = require('./connections');
+const connetions = require("./connections");
 
+const getTestimonial = async (req, res) => {
+  await connetions.query("SELECT * FROM testimonials", (err, results) => {
+    if (err) return res.status(500).json({ message: err });
+    return res.status(200).json({ message: results });
+  });
+};
 
-const getTestimonial = async(req,res)=>{
-    await connetions.query('SELECT * FROM testimonials',(err,results)=>{
-        if(err) return res.status(500).json({message:err});
-        return res.status(200).json({message:results});
-    })
-}
+const postTestimonial = async (req, res) => {
+  const { name, subject, company_name,role } = req.body;
 
+  await connetions.query(
+    "INSERT INTO testimonials(name,subject,company_name,role) values (?,?,?,?)",
+    [name, subject, company_name,role],
+    (err, results) => {
+      if (err) return res.status(500).json({ message: err });
+      return res.status(200).json({ message: "data inserted successfully." });
+    }
+  );
+};
 
-const postTestimonial = async(req,res)=>{
-    const{name,subject,compony_name} = req.body;
-
-    await connetions.query('INSERT INTO testimonials(name,subject,compony_name) values (?,?,?)',[name,subject,compony_name],(err,results)=>{
-        if(err) return res.status(500).json({message:err});
-        return res.status(200).json({message:"data inserted successfully."});
-    })
-}
-
-
-module.exports = {getTestimonial,postTestimonial}
+module.exports = { getTestimonial, postTestimonial };
